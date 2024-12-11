fc0f8776b61da3564824dc8821d9dde17b940c07
subinth <a6929ab@naver.com>
2024-12-11 18:59:27 +0900

    mypage 메인

---
+++ dream-backend/src/main/dream-frontend/src/my-page/Sidebar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // react-router-dom을 이용해 네비게이션
+
+function Sidebar() {
+    return (
+        <div className="mypage-sidebar">
+            <ul className="menu">
+                <h2>마이 페이지</h2>
+                <h3>쇼핑 정보</h3>
+                <li><Link to="/my-page/buys-history">구매 내역</Link></li>
+                <li><Link to="/my-page/sales-history">판매 내역</Link></li>
+                <li><Link to="/my-page/interests">관심</Link></li>
+            </ul>
+            <ul className="menu">
+                <h3>내 정보</h3>
+                <li><Link to="/my-page/information">로그인 정보</Link></li>
+                <li><Link to="/my-page/profile">프로필 관리</Link></li>
+                <li><Link to="/my-page/address">주소록</Link></li>
+                <li><Link to="/my-page/account">판매 정산 계좌</Link></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Sidebar;
